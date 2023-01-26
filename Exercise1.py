21d620fcd2fe75b399a751d07cf3cec40e6cd765
eddie-sayer <79141499+eddie-sayer@users.noreply.github.com>
2023-01-26 20:52:04 +0000

    q1_error_loop

--- Exercise1.py
+++ Exercise1.py
@@ -1,3 +1,5 @@
+from math import pi
+
 def Leibniz(n):
     approx = 0
     for i in range(n):
@@ -9,3 +11,17 @@ def Leibniz(n):
 approx_100 = Leibniz(100)
 approx_1000 = Leibniz(1000)
 approx_10000 = Leibniz(10000)
+
+error_100 = abs(pi - approx_100)
+error_1000 = abs(pi - approx_1000)
+error_10000 = abs(pi - approx_10000)
+
+print(error_100, error_1000, error_10000)
+
+error = 1
+n = 10000
+while error > 10**(-7):
+    n += n+1
+    error = abs(pi - Leibniz(n))
+
+print(n)
