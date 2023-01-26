2d0ee3aad79b696e407fefe73ab2fd838d4fb917
eddie-sayer <79141499+eddie-sayer@users.noreply.github.com>
2023-01-26 20:31:26 +0000

    final_q1_a

--- Exercise1.py
+++ Exercise1.py
@@ -4,5 +4,8 @@ def Leibniz(n):
         approx += 8/((4*i+1)*(4*i+3))
     return approx
 
-print(Leibniz(1000))
+#print(Leibniz(1000))
 
+approx_100 = Leibniz(100)
+approx_1000 = Leibniz(1000)
+approx_10000 = Leibniz(10000)
